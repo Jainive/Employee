671153dfd8010e19892355415847b3a2b0227034
Jainive <victoriousnivi@gmail.com>
2022-04-02 11:12:14 +0530

    Committed

---
+++ org/add/GreensTech.java
@@ -0,0 +1,36 @@
+package org.add;
+
+public class GreensTech {
+	public void greensOmr() {
+		System.out.println("Greens Tech \n OMR\n Chennai");
+		
+	}
+	public void greensAdayar() {
+		System.out.println("Greens Tech \n Adayar\n Chennai");
+	}
+	public void greensTambaram() {
+		System.out.println("\nGreens Tech \n Tambaram \n Chennai");
+	}
+	public void greensVelachery() {
+		System.out.println("\nGreens Tech \n Velachery \n Chennai");
+	}
+	public void greensAnnanagar() {
+		System.out.println("\nGreens Tech \n Annanagar \n Chennai");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GreensTech greensOmr= new GreensTech();
+		GreensTech greensAdayar=new GreensTech();
+		GreensTech greensTambaram =new GreensTech();
+		GreensTech greensVelachery = new GreensTech();
+		GreensTech greensAnnanagar=new GreensTech();
+		greensOmr.greensOmr();
+		greensAdayar.greensAdayar();
+		greensVelachery.greensVelachery();
+		greensAnnanagar.greensAnnanagar();
+		greensTambaram.greensTambaram();
+		
+		
+	}
+
+}
